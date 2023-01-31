59d1aac5ecfc310e1ea6602290552ac92733d022
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2023-01-31 16:28:45 -0300

    Definido um Tema para aplicação e  aplicado as fontes

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -4,7 +4,7 @@ import '../models/category.dart';
 class CategoryItem extends StatelessWidget {
   final Category category;
 
-  CategoryItem(this.category, {super.key});
+  const CategoryItem(this.category, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +21,10 @@ class CategoryItem extends StatelessWidget {
           end: Alignment.bottomRight,
         ),
       ),
-      child: Text(category.title),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.titleLarge,
+      ),
     );
   }
 }
