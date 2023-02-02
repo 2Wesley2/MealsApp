fdb4364aaf0cbc5483b7f6bc7d9ef9e4658f6eb3
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2023-02-02 12:56:06 -0300

    Renderizado a lista de ingredientes na tela de detalhes e feito a estilização no componente

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -29,6 +29,32 @@ class MealDetailScreen extends StatelessWidget {
               style: Theme.of(context).textTheme.titleLarge,
             ),
           ),
+          Container(
+            width: 300,
+            height: 200,
+            padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ListView.builder(
+              itemCount: meal.ingredients.length,
+              itemBuilder: (ctx, index) {
+                return Card(
+                  color: Theme.of(context).colorScheme.secondary,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 10,
+                    ),
+                    child: Text(meal.ingredients[index]),
+                  ),
+                );
+              },
+            ),
+          )
         ],
       ),
     );
