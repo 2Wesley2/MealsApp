cbc27040a44d517129051f328567a411038c9b90
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2023-02-07 13:33:41 -0300

    Passado as informações da lista de comidas para o Main e atráves dele, é passado a lista via construtror para tela de categorias

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,22 @@ import 'package:meals/screens/tabs_screen.dart';
 import 'screens/categories_meals_screen.dart';
 import 'utils/app_routes.dart';
 import 'screens/settings_screen.dart';
+import 'models/meal.dart';
+import 'data/dummy_data.dart';
 
 void main() {
   runApp(const MealsApp());
 }
 
-class MealsApp extends StatelessWidget {
+class MealsApp extends StatefulWidget {
   const MealsApp({Key? key}) : super(key: key);
 
+  @override
+  State<MealsApp> createState() => _MealsAppState();
+}
+
+class _MealsAppState extends State<MealsApp> {
+  final List<Meal> _availableMeals = dummyMeals;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -35,7 +43,8 @@ class MealsApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         AppRoutes.HOME: (ctx) => TabsScreen(),
-        AppRoutes.CATEGORIES_MEALS: (ctx) => const CategoriesMealsScreen(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) =>
+            CategoriesMealsScreen(_availableMeals),
         AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
         AppRoutes.SETTINGS: (ctx) => SettingsScreen(),
       },
