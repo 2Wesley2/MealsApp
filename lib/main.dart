b4e257308d7d5747cbfc36b40206b0f473ca0f19
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2023-02-08 12:15:37 -0300

    remove ignore const in this file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:meals/models/settings.dart';
 import 'package:meals/screens/meal_detail_screen.dart';
